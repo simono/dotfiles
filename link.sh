15a9458ff5c7e859bfb8a3314fcd9bd3e07ad0f8
Simon Olofsson <simon@olofsson.de>
2011-11-14 16:37:31 +0100

    link.sh: Add option to force link.

--- link.sh
+++ link.sh
@@ -8,12 +8,16 @@
 set -o errexit
 set -o nounset
 
-full_path="$(pwd)/$(dirname $0)"
+local full_path="$(pwd)/$(dirname $0)"
 
-ln -vs $full_path/ack/ackrc		$HOME/.ackrc
-ln -vs $full_path/git/gitconfig		$HOME/.gitconfig
-ln -vs $full_path/hg/hgrc		$HOME/.hgrc
-ln -vs $full_path/tmux/tmux.conf	$HOME/.tmux.conf
-ln -vs $full_path/vim/vimrc		$HOME/.vimrc
-ln -vs $full_path/zsh/zprofile		$HOME/.zprofile
-ln -vs $full_path/zsh/zshrc		$HOME/.zshrc
+getopts f force && shift $((OPTIND-1))
+test $force = 'f' || force=''
+local ln="ln -vs$force"
+
+$ln $full_path/ack/ackrc	$HOME/.ackrc
+$ln $full_path/git/gitconfig	$HOME/.gitconfig
+$ln $full_path/hg/hgrc		$HOME/.hgrc
+$ln $full_path/tmux/tmux.conf	$HOME/.tmux.conf
+$ln $full_path/vim/vimrc	$HOME/.vimrc
+$ln $full_path/zsh/zprofile	$HOME/.zprofile
+$ln $full_path/zsh/zshrc	$HOME/.zshrc
