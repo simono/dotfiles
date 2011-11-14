72be2f5a1436047ca219e50177a99f83ff5fad6a
Simon Olofsson <simon@olofsson.de>
2011-11-14 16:16:29 +0100

    link.sh: Update

--- link.sh
+++ link.sh
@@ -10,4 +10,10 @@ set -o nounset
 
 full_path="$(pwd)/$(dirname $0)"
 
-ln -vs $full_path/git/gitconfig $HOME/.gitconfig
+ln -vs $full_path/ack/ackrc		$HOME/.ackrc
+ln -vs $full_path/git/gitconfig		$HOME/.gitconfig
+ln -vs $full_path/hg/hgrc		$HOME/.hgrc
+ln -vs $full_path/tmux/tmux.conf	$HOME/.tmux.conf
+ln -vs $full_path/vim/vimrc		$HOME/.vimrc
+ln -vs $full_path/zsh/zprofile		$HOME/.zprofile
+ln -vs $full_path/zsh/zshrc		$HOME/.zshrc
