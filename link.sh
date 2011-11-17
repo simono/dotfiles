1f7214127f2fe9ae17c5345c4fac74d77c0260f8
Simon Olofsson <simon@olofsson.de>
2011-11-17 14:50:26 +0100

    link.sh: Remove force option, make linking smarter instead.

--- link.sh
+++ link.sh
@@ -8,16 +8,24 @@
 set -o errexit
 set -o nounset
 
-full_path="$(pwd)/$(dirname $0)"
+# link target linkName
+link() {
+	local target="$(pwd)/$(dirname $0)/$1"
+	local linkName="$HOME/$2"
 
-getopts f force && shift $((OPTIND-1))
-test $force = 'f' || force=''
-ln="ln -vs$force"
+	if [ -L $linkName ]; then
+		ln -vsf $target $linkName
+	elif [ ! -e $linkName ]; then
+		ln -vs $target $linkName
+	else
+		echo "$linkName already exists!"
+	fi
+}
 
-$ln $full_path/ack/ackrc	$HOME/.ackrc
-$ln $full_path/git/gitconfig	$HOME/.gitconfig
-$ln $full_path/hg/hgrc		$HOME/.hgrc
-$ln $full_path/tmux/tmux.conf	$HOME/.tmux.conf
-$ln $full_path/vim/vimrc	$HOME/.vimrc
-$ln $full_path/zsh/zprofile	$HOME/.zprofile
-$ln $full_path/zsh/zshrc	$HOME/.zshrc
+link ack/ackrc		.ackrc
+link git/gitconfig	.gitconfig
+link hg/hgrc		.hgrc
+link tmux/tmux.conf	.tmux.conf
+link vim/vimrc		.vimrc
+link zsh/zprofile	.zprofile
+link zsh/zshrc		.zshrc
