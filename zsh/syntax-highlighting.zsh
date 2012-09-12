900da39567d9c8912df5b4dc071570f403f7e63f
Simon Olofsson <simon@olofsson.de>
2012-09-12 15:13:38 +0200

    zsh: Add configuration for syntax highlighting.

---
+++ zsh/syntax-highlighting.zsh
@@ -0,0 +1,15 @@
+# Zsh syntax highlighting.
+#
+# Location: ~/.zsh/syntax-highlighting.zsh
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+
+ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
+
+dangerous_cmds=('rm' 'sudo')
+for i in $dangerous_cmds; do
+	ZSH_HIGHLIGHT_PATTERNS+=($i 'fg=white,bold,bg=red')
+done
