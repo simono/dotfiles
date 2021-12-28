bed70567fb28f8abafa58932b3e0a0b111562a35
Simon Olofsson <simon@olofsson.de>
2021-12-28 21:04:51 +0100

    Split setup and link scripts.

---
+++ link.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+# Link config files.
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+set -o errexit
+set -o nounset
+
+current_dir="$(dirname $0)"
+
+# link target linkName
+link() {
+	local target="$(pwd)/$current_dir/$1/$2"
+	local linkName="$HOME/.$2"
+
+	if [ ! -r $target ]; then
+		return
+	elif [ -L $linkName ]; then
+		ln -nvsf $target $linkName
+	elif [ ! -e $linkName ]; then
+		ln -nvs $target $linkName
+	else
+		echo "$linkName already exists!"
+	fi
+}
+
+link ack	ackrc
+link git	gitconfig
+link tmux	tmux.conf
+mkdir -p $HOME/.tmux
+link ''		tmux/tmux-colors-solarized
+link ''		tmux/tmux-sensible
+mkdir -p $HOME/.vim/swap
+mkdir -p $HOME/.vim/backup
+mkdir -p $HOME/.vim/undo
+mkdir -p $HOME/.vim/pack/my-plugins
+link ''		vim/pack/my-plugins/start
+link vim	vimrc
+link zsh	zprofile
+link zsh	zshrc
+link ''		zsh
+
+if [ $(uname -s) = 'Darwin' ]; then
+	link ''		Brewfile
+	link ''		"Brewfile-$(hostname -s)"
+	link ''		terminal
+fi
--- setup.sh
+++ setup.sh
@@ -10,52 +10,14 @@ set -o nounset
 
 current_dir="$(dirname $0)"
 
-# link target linkName
-link() {
-	local target="$(pwd)/$current_dir/$1/$2"
-	local linkName="$HOME/.$2"
-
-	if [ ! -r $target ]; then
-		return
-	elif [ -L $linkName ]; then
-		ln -nvsf $target $linkName
-	elif [ ! -e $linkName ]; then
-		ln -nvs $target $linkName
-	else
-		echo "$linkName already exists!"
-	fi
-}
-
-(cd $current_dir && git checkout main && git pull && git submodule update --init)
-
-link ack	ackrc
-link git	gitconfig
-link tmux	tmux.conf
-mkdir -p $HOME/.tmux
-link ''		tmux/tmux-colors-solarized
-link ''		tmux/tmux-sensible
-mkdir -p $HOME/.vim/swap
-mkdir -p $HOME/.vim/backup
-mkdir -p $HOME/.vim/undo
-mkdir -p $HOME/.vim/pack/my-plugins
-link ''		vim/pack/my-plugins/start
-link vim	vimrc
-link zsh	zprofile
-link zsh	zshrc
-link ''		zsh
+(cd $current_dir && git checkout main && git pull && git submodule update --init && ./link.sh)
 
 if [ $(uname -s) = 'Darwin' ]; then
 	caffeinate -w $$ &
 
-	link ''		Brewfile
-	link ''		terminal
-
-	localBrewfile="Brewfile-$(hostname -s)"
-	link ''		$localBrewfile
-
 	brew bundle install --global --no-lock
-	localBrewfilePath="$HOME/.$localBrewfile"
-	test -r $localBrewfilePath && brew bundle install --file=$localBrewfilePath --no-lock
+	localBrewfile="$HOME/.Brewfile-$(hostname -s)"
+	test -r $localBrewfile && brew bundle install --file=$localBrewfile --no-lock
 
 	brew upgrade
 	brew cleanup
