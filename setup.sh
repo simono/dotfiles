1295b52bfa08747722b8fd58785019e6bf3f95ec
Simon Olofsson <simon@olofsson.de>
2023-04-28 18:59:29 +0200

    setup: Ignore pinned formula during upgrade.

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ if [ $(uname -s) = 'Darwin' ]; then
 	localBrewfile="$HOME/.Brewfile-$(hostname -s)"
 	test -r $localBrewfile && brew bundle install --file=$localBrewfile --no-lock
 
-	brew upgrade
+	brew upgrade --ignore-pinned
 	brew cleanup
 
 	clear
