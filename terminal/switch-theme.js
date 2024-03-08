d693359b8389658733d6074b5ea7d8df6ebf066a
Simon Olofsson <simon@olofsson.de>
2024-03-08 19:34:38 +0100

    terminal: add some basic error handling

--- terminal/switch-theme.js
+++ terminal/switch-theme.js
@@ -4,4 +4,9 @@ const appearancePreferences = Application('System Events').appearancePreferences
 const settings = `rose-pine-${appearancePreferences.darkMode() ? 'moon' : 'dawn'}`;
 
 const terminal = Application('Terminal');
-terminal.windows().forEach(w => w.tabs().forEach(t => t.currentSettings = terminal.settingsSets.byName(settings)));
+try {
+	terminal.windows().forEach(w => w.tabs().forEach(t => t.currentSettings = terminal.settingsSets.byName(settings)));
+} catch (e) {
+	// Just log the error
+	console.log(e);
+}
