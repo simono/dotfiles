6d7a47c0c96a91b506245efe8ebd933d55bbc260
Simon Olofsson <simon@olofsson.de>
2024-03-09 15:41:35 +0100

    zsh: fix path to Homebrew command-not-found

--- zsh/02_systems.zsh
+++ zsh/02_systems.zsh
@@ -7,7 +7,7 @@
 
 case $(uname -s) in
 	'Darwin' )
-		local hb_cnf_handler="$(brew --prefix)/Homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
+		local hb_cnf_handler="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
 		if [ -f "$hb_cnf_handler" ]; then
 			source "$hb_cnf_handler";
 		fi
