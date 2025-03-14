17474d8adee07e6c16a3a32585d7ff9347280121
Simon Olofsson <simon@olofsson.de>
2025-03-15 00:17:35 +0100

    fish: add config for ripgrep (rg)

---
+++ config/fish/conf.d/rg.fish
@@ -0,0 +1,9 @@
+# Fish config for ripgrep (rg).
+#
+# Location: ~/.config/fish/conf.d/rg.fish
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+# Config file for ripgrep
+set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
