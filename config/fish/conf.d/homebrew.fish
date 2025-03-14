e8c92307d28e08b3b66661418ddbca6d873d2c67
Simon Olofsson <simon@olofsson.de>
2025-03-14 23:44:21 +0100

    fish: add config for homebrew

---
+++ config/fish/conf.d/homebrew.fish
@@ -0,0 +1,12 @@
+# Fish config for Homebrew.
+#
+# Location: ~/.config/fish/conf.d/homebrew.fish
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+# Homebrew Command Not Found
+set -l __hb_cnf_handler (brew --repository)"/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
+if test -f $__hb_cnf_handler
+    source $__hb_cnf_handler
+end
