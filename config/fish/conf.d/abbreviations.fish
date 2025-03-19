05f70852ab5692ee0021bba872a478213a6145c6
Simon Olofsson <simon@olofsson.de>
2025-03-19 08:28:24 +0100

    fish: add config for abbrevations

---
+++ config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,13 @@
+# Fish config for commonly used abbrevations.
+#
+# Location: ~/.config/fish/conf.d/abbreviations.fish
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+abbr -a -- cat bat
+abbr -a -- cp 'cp -iv'
+abbr -a -- df 'df -h'
+abbr -a -- du 'du -h'
+abbr -a -- mv 'mv -iv'
+abbr -a -- rm 'rm -Iv'
