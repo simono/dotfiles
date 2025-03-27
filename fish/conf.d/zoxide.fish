19538705693ed6a1dd717c9d721df12ea450080b
Simon Olofsson <simon@olofsson.de>
2025-03-27 17:12:54 +0100

    fish: add config for zoxide

---
+++ fish/conf.d/zoxide.fish
@@ -0,0 +1,10 @@
+# Fish config for zoxide.
+#
+# Location: ~/.config/fish/conf.d/zoxide.fish
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+# Initialize zoxide
+# use `j` as command
+zoxide init --cmd j fish | source
