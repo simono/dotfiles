7c8eb2691532d759e6c0c7586d0b825cb59968ec
Simon Olofsson <simon@olofsson.de>
2025-03-29 00:02:07 +0100

    delta: configure Rosé Pine as theme

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -10,8 +10,10 @@
 function __so_choose_theme --on-event fish_prompt
     if test "$(defaults read -g AppleInterfaceStyle 2> /dev/null)" = "Dark"
         fish_config theme choose "Rosé Pine Moon"
+        set -gx DELTA_FEATURES "+dark"
     else
         fish_config theme choose "Rosé Pine Dawn"
+        set -gx DELTA_FEATURES "+light"
     end
     # Make the pager background transparent
     set -e fish_pager_color_background
