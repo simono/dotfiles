a12eb5700b3d6eb148f5ccc94f11e65015f775cc
Simon Olofsson <simon@olofsson.de>
2025-03-30 00:10:07 +0100

    bat: fix syntax highlighting for man pages

--- fish/conf.d/bat.fish
+++ fish/conf.d/bat.fish
@@ -15,4 +15,5 @@ abbr -a --position anywhere -- --help '--help | bat -plhelp'
 set -gx LESS "--ignore-case --no-histdups --color=s+y"
 
 # Use bat as manpager
-set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+# Reset the theme because Rosé Pine doesn't support highlighting for man pages.
+set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman --theme default'"
