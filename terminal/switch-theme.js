e9ea29e9a13229855c3d5894d3c3bc47ced36479
Simon Olofsson <simon@olofsson.de>
2025-03-31 20:36:54 +0200

    terminal: format JavaScript

--- terminal/switch-theme.js
+++ terminal/switch-theme.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env osascript -l JavaScript
 
-const appearancePreferences = Application('System Events').appearancePreferences;
-const settings = `rose-pine-${appearancePreferences.darkMode() ? 'moon' : 'dawn'}`;
+const appearancePreferences =
+  Application("System Events").appearancePreferences;
+const settings = `rose-pine-${appearancePreferences.darkMode() ? "moon" : "dawn"}`;
 
-const terminal = Application('Terminal');
+const terminal = Application("Terminal");
 try {
-	terminal.windows().forEach(w => w.tabs().forEach(t => t.currentSettings = terminal.settingsSets.byName(settings)));
+  terminal
+    .windows()
+    .forEach((w) =>
+      w
+        .tabs()
+        .forEach(
+          (t) => (t.currentSettings = terminal.settingsSets.byName(settings)),
+        ),
+    );
 } catch (e) {
-	// Just log the error
-	console.log(e);
+  // Just log the error
+  console.log(e);
 }
