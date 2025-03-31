abcb6f03610d1c1d1a452cc33aba7f2654631a8f
Simon Olofsson <simon@olofsson.de>
2025-03-31 14:52:37 +0200

    bat: fix theme for manpages

--- fish/conf.d/bat.fish
+++ fish/conf.d/bat.fish
@@ -15,5 +15,5 @@ abbr -a --position anywhere -- --help '--help | bat -plhelp'
 set -gx LESS "--ignore-case --no-histdups --color=s+y"
 
 # Use bat as manpager
-# Reset the theme because Rosé Pine doesn't support highlighting for man pages.
-set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman --theme default'"
+# Choose another theme because Rosé Pine doesn't support highlighting for man pages.
+set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman --theme auto:system --theme-light \"Solarized (light)\" --theme-dark \"Solarized (dark)\"'"
