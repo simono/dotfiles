48a233aad9339df6f147863ec6be6cd2210bceb0
Simon Olofsson <simon@olofsson.de>
2025-04-01 10:33:39 +0200

    terminal: get the theme as argument and run in background

--- terminal/switch-theme.js
+++ terminal/switch-theme.js
@@ -1,21 +1,19 @@
 #!/usr/bin/env osascript -l JavaScript
 
-const appearancePreferences =
-  Application("System Events").appearancePreferences;
-const settings = `rose-pine-${appearancePreferences.darkMode() ? "moon" : "dawn"}`;
-
-const terminal = Application("Terminal");
-try {
-  terminal
-    .windows()
-    .forEach((w) =>
-      w
-        .tabs()
-        .forEach(
-          (t) => (t.currentSettings = terminal.settingsSets.byName(settings)),
-        ),
-    );
-} catch (e) {
-  // Just log the error
-  console.log(e);
+function run(theme) {
+  const terminal = Application("Terminal");
+  try {
+    terminal
+      .windows()
+      .forEach((w) =>
+        w
+          .tabs()
+          .forEach(
+            (t) => (t.currentSettings = terminal.settingsSets.byName(theme)),
+          ),
+      );
+  } catch (e) {
+    // Just log the error
+    console.log(e);
+  }
 }
