3c6acd438a4f7d7826642e593cec6ba5f84c4977
Simon Olofsson <simon@olofsson.de>
2025-04-02 17:00:08 +0200

    fish: remove `HOMEBREW_CASK_OPTS`
    I set this locally where needed.

--- fish/conf.d/homebrew.fish
+++ fish/conf.d/homebrew.fish
@@ -5,9 +5,6 @@
 # Simon Olofsson <simon@olofsson.de>
 #
 
-# Install Fonts for all users
-set -gx HOMEBREW_CASK_OPTS "--fontdir=/Library/Fonts"
-
 # Homebrew Command Not Found
 set -l __hb_cnf_handler (brew --repository)"/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
 if test -f $__hb_cnf_handler
