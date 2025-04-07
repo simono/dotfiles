d7ebac06e131479f62dd4ed7a8f766283098c59c
Simon Olofsson <simon.olofsson@ionos.com>
2025-04-07 11:56:45 +0200

    dotbot: refine dotbot functions and config
    Split config into three files (sync, config and update). This way they
    can be run separately and running sync before anything else ensures that
    all files are up-to-date.
    
    Use only one function for Fish to update dotfiles. The others can
    be configured locally.

---
+++ fish/functions/so-dotfiles-update.fish
@@ -0,0 +1,12 @@
+# Fish function to update dotfiles with Dotbot.
+#
+# Location: ~/.config/fish/functions/so-dotfiles-update.fish
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+function so-dotfiles-update --description 'Update dotfiles with Dotbot'
+    dotbot -q -c $HOME/.dotfiles/dotbot-sync.yaml
+    dotbot -q -c $HOME/.dotfiles/dotbot-config.yaml
+    dotbot -c $HOME/.dotfiles/dotbot-update.yaml
+end
