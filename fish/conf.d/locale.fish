b64b99dd783efa49d47fbd636d7c826a0d8c50d5
Simon Olofsson <simon.olofsson@ionos.com>
2025-04-08 19:42:59 +0200

    fish: don't set LANG, Ghostty sets this automatically

--- fish/conf.d/locale.fish
+++
@@ -1,9 +0,0 @@
-# Fish config for locale settings.
-#
-# Location: ~/.config/fish/conf.d/locale.fish
-#
-# Simon Olofsson <simon@olofsson.de>
-#
-
-# Use en_US with UTF-8 encoding
-set -gx LANG "en_US.UTF-8"
