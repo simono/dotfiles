a81b53aeff88b889aab8c422be90b89567b0b183
Simon Olofsson <simon@olofsson.de>
2025-04-11 13:38:13 +0200

    fish: turn git aliases into abbrevations

---
+++ fish/conf.d/git.fish
@@ -0,0 +1,18 @@
+# Fish config for git abbrevations.
+#
+# Location: ~/.config/fish/conf.d/git.fish
+#
+# Simon Olofsson <simon@olofsson.de>
+#
+
+abbr g 'git'
+abbr ga 'git add'
+abbr gc 'git commit'
+abbr gd 'git diff'
+abbr gdc 'git diff --cached'
+abbr gl 'git log'
+abbr glg 'git log --graph --all'
+abbr grs 'git restore --staged'
+abbr gs 'git status'
+abbr gsh 'git show'
+abbr gsw 'git switch'
