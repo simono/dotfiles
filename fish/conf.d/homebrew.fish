9eab87e6ef783b8a46b3ad7e3314cbb2c9b0f01c
Simon Olofsson <simon@olofsson.de>
2025-05-07 22:31:28 +0200

    fish: remove shell workaround
    Dotbot runs the commands with `$SHELL`. This is the login shell for the
    current user and not the current shell.
    `rustup` should be installed everywhere and fisher is now explicitly run
    with fish.

--- fish/conf.d/homebrew.fish
+++ fish/conf.d/homebrew.fish
@@ -8,9 +8,6 @@
 # Setup shell for Homebrew
 /opt/homebrew/bin/brew shellenv | source
 
-# Somehow SHELL is not set correctly in fish
-set -gx SHELL (type -p fish)
-
 # Homebrew Command Not Found
 set -l __hb_cnf_handler (brew --repository)"/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
 if test -f $__hb_cnf_handler
