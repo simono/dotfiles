9a347fa50aa9c3eb88a9d713955f963ed3b79517
Simon Olofsson <simon@olofsson.de>
2025-07-23 11:42:29 +0200

    fish: fix quoting

--- fish/conf.d/fzf-fish.fish
+++ fish/conf.d/fzf-fish.fish
@@ -9,7 +9,7 @@
 set -g fzf_preview_dir_cmd l --all --color=always --oneline
 
 # Open files and directories in the editor.
-set -g fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+set -g fzf_directory_opts --bind 'ctrl-o:execute($EDITOR {} &> /dev/tty)'
 
 # fzf.fish binds everything to normal and insert mode.
 # That's not what I want.
