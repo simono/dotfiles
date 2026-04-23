0f87befcad9764435f871b4b6b240902eabb93b7
Simon Olofsson <simon@olofsson.de>
2026-04-23 11:13:17 +0200

    fish: simplify pure k8s check to one-liner

--- fish/conf.d/prompt.fish
+++ fish/conf.d/prompt.fish
@@ -8,8 +8,4 @@
 # Use fish-async-prompt to avoid lag from git status.
 set -g async_prompt_functions _pure_prompt_git
 
-if command -q kubectl
-    set --universal pure_enable_k8s true
-else
-    set --universal pure_enable_k8s false
-end
+set -g pure_enable_k8s (command -q kubectl; and echo true; or echo false)
